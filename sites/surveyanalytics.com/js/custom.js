b31e1d47d156457baa1aec116e54011b182d2818
Juan Pablo Manson <jmanson@bracketmedia.com>
2012-10-22 18:57:45 -0300

    Front-end files

---
+++ sites/surveyanalytics.com/js/custom.js
@@ -0,0 +1,14 @@
+/* $(document).ready(function() {               
+	$("#resources").click(function(event) { 
+		event.preventDefault();		
+		$(".currentSelected").addClass("currentSelected");			
+		$(".currentSubSelected").removeClass("currentSubSelected");			
+	}).click();
+
+	$(".currentSelected").click(function(event) {
+		event.preventDefault();	
+		$(".currentSelected").removeClass("currentSelected");			
+		$(".currentSubSelected").addClass(".currentSubSelected");			
+	});
+
+}); */
